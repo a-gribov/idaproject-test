9b249fbc20389eacaca137e312315d7aaccb6653
agry <llununull@gmail.com>
2024-06-20 22:11:28 +0300

    feat: gh pages

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,9 @@
 export default {
+    target: 'static',
+    router: {
+      base: '/idaproject-test/'
+    },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'idaproject-test',
